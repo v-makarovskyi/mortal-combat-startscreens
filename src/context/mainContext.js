55be450494c39e5b03043de22486ec4d1f4a3ed1
Vladimir <bigcorvet@gmail.com>
2023-09-26 21:05:55 +0300

    implemented context module

---
+++ src/context/mainContext.js
@@ -0,0 +1,21 @@
+import React, {createContext, useCallback, useState} from "react";
+
+export const MainContext = createContext({})
+
+export const MainContextProvider = ({ children }) => {
+    const [selectedPlayers, setSelectedPlayers] = useState({
+        player_1: null,
+        player_2: null,
+    })
+    const [activeScreen, setActiveScreen] = useState(0)
+
+    const selectPlayer = ({ character, characterNumber=1 }) => {
+        setSelectedPlayers((prev) => ({...prev, [`player_${characterNumber}`]: character}))
+    }
+
+    return (
+        <MainContext.Provider value={{selectedPlayers, selectPlayer, activeScreen, setActiveScreen}}>
+            { children }
+        </MainContext.Provider>
+    )
+}
