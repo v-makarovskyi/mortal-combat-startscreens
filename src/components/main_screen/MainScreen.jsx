8df6dc3e4ee618fca5801a5c39239b5990db559e
Vladimir <bigcorvet@gmail.com>
2023-09-27 19:36:23 +0300

    added func handlePlayers

--- src/components/main_screen/MainScreen.jsx
+++ src/components/main_screen/MainScreen.jsx
@@ -4,21 +4,25 @@ import SelectedCharacters from "../SelectedCharacters";
 import styles from "./MainScreen.module.css";
 
 export default function MainScreen() {
+    const handlePlayers = (players) => {
+        return (
+            <SelectedCharacters
+                players={players}
+                classes={{
+                    left: styles.player_1,
+                    right: styles.player_2,
+                }}
+            />
+        )
+    }
+
   return (
     <div className={styles.mainScreen}>
       <div className={styles.container}>
         <h2 className={styles.title}>Select your fighter</h2>
         <div className={styles.content}>
           <CharactersList>
-            {(players) => (
-              <SelectedCharacters
-                players={players}
-                classes={{
-                  left: styles.player_1,
-                  right: styles.player_2,
-                }}
-              />
-            )}
+           { handlePlayers }
           </CharactersList>
         </div>
         <h3 className={styles.bottom_title}>Kombat zone: soul chamber</h3>
