aa72e6122ca66be51b30ed09fceb0b7974cb2968
Vladimir <bigcorvet@gmail.com>
2023-09-28 21:26:50 +0300

    added popup component

--- src/components/vs_screen/VersusCodesPopup.jsx
+++ src/components/vs_screen/VersusCodesPopup.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { forwardRef, useCallback, useRef, useState } from "react";
+import { versusCodesSymbols } from "../../data";
+import styles from "./vsScreen.module.css";
 
-export default function VersusCodesPopup() {
+const winnerCodes = [311111, 112111, 111113];
+
+const VersusCodesPopup = ({ code }) => {
+  
   return (
-    <div>VersusCodesPopup</div>
+    <div className={styles.popup}>
+        <p>
+        You win.<br /> our winning code: "{code}
+        Congratulations!
+        </p>
+    </div>
   )
-}
+};
+
+export default VersusCodesPopup;
