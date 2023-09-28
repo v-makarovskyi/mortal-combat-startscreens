cf11c6145b5ffa1f4253f71ed51f1f46f00d68c1
Vladimir <bigcorvet@gmail.com>
2023-09-28 19:55:21 +0300

    added keyDownHandler func

--- src/components/vs_screen/VersusCodes.jsx
+++ src/components/vs_screen/VersusCodes.jsx
@@ -1,22 +1,61 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react'
-import { versusCodesSymbols } from '../../data'
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  Fragment,
+} from "react";
+import { versusCodesSymbols } from "../../data";
+import VersusCodesPopup from "./VersusCodesPopup";
+import VersusCodesItem from "./VersusCodesItem";
+import styles from "./vsScreen.module.css";
 
-const accessKeys = ['q', 'w', 'e', 'r', 't', 'y']
+const REQUIRED_LETTERS = ["q", "w", "e", "r", "t", "y"];
 
 export default function VersusCodes() {
-    const [selectedSymbol, setSelectedSymbol] = useState(
-        accessKeys.map((item) => ({ id: item, symbolIdx: 0 }))
-    )
-   const codesPopupRef = useRef(null)
+  const icons = REQUIRED_LETTERS.map((item) => ({ id: item, iconIdx: 0 }));
+  const [selectedIcons, setSelectedIcons] = useState(icons);
 
-   const keyDownHandler = () => {}
+  const keyDownHandler = useCallback(
+    (e) => {
+      const key = e.key.toLowerCase();
+      if (!REQUIRED_LETTERS.includes(key)) return;
 
-   useEffect(() => {
-    document.addEventListener('keydown', keyDownHandler)
-    return () => document.removeEventListener('keydown', keyDownHandler)
-   }, [keyDownHandler])
+      const nextSelectedIcons = selectedIcons.map((item) => {
+        if (item.id === key) {
+          return {
+            ...item,
+            iconIdx:
+              item.iconIdx < versusCodesSymbols.length - 1
+                ? item.iconIdx + 1
+                : 0,
+          };
+        }
+        return item;
+      });
+      setSelectedIcons(nextSelectedIcons);
+    },
+    [selectedIcons]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyDownHandler);
+    return () => document.removeEventListener("keydown", keyDownHandler);
+  }, [keyDownHandler]);
 
   return (
-    <div>VersusCodes</div>
-  )
+    <Fragment>
+      <div className={styles.versus_codes}>
+        {
+            REQUIRED_LETTERS.map((_, index) => (
+                <VersusCodesItem 
+                    key={index}
+                    symbol={versusCodesSymbols[selectedIcons[index].iconIdx]}
+                />
+            ))
+        }
+      </div> 
+      <VersusCodesPopup />
+    </Fragment>
+  );
 }
