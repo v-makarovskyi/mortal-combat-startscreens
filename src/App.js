52aa3a6a50ef2054ac0f57fce81bafcf14d6de69
Vladimir <bigcorvet@gmail.com>
2023-09-28 11:33:10 +0300

    added logic for transition between screens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React, { useContext } from "react";
+import MainScreen from "./components/main_screen/MainScreen";
+import VsScreen from "./components/vs_screen/VsScreen";
+import { MainContext } from "./context/mainContext";
+
 function App() {
+  const { activeScreen } = useContext(MainContext)
+  const screens = [<MainScreen />, <VsScreen/>]
+
   return (
-    <div className="App">App</div>
+    <div className="App">
+      {screens[activeScreen]}
+    </div>
   );
 }
 
