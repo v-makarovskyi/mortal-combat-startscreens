00e2c10fa33f7e2227df66f9ecf1f81e2619bb69
Vladimir <bigcorvet@gmail.com>
2023-09-28 21:58:51 +0300

    added message

--- src/components/vs_screen/VersusCodesPopup.jsx
+++ src/components/vs_screen/VersusCodesPopup.jsx
@@ -2,15 +2,13 @@ import React, { forwardRef, useCallback, useRef, useState } from "react";
 import { versusCodesSymbols } from "../../data";
 import styles from "./vsScreen.module.css";
 
-const winnerCodes = [311111, 112111, 111113];
-
 const VersusCodesPopup = ({ code }) => {
   
   return (
     <div className={styles.popup}>
         <p>
-        You win.<br /> our winning code: "{code}
         Congratulations!
+        You win.<br /> our winning code: {code}
         </p>
     </div>
   )
