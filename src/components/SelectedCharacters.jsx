cba879f0753d91dda936b501d58f7cdf41aa2656
Vladimir <bigcorvet@gmail.com>
2023-09-28 13:46:22 +0300

    updated names props

--- src/components/SelectedCharacters.jsx
+++ src/components/SelectedCharacters.jsx
@@ -4,10 +4,10 @@ export default function SelectedCharacters({ classes, players }) {
   return (
     <>
         <div className={classes.left}>
-            <img src={`characters/large/${players.playerOne?.image_large}`} alt='player_1' />
+            <img src={`characters/large/${players.player_1?.image_large}`} alt='player_1' />
         </div>
         <div className={classes.right}>
-            <img src={`characters/large/${players.playerTwo?.image_large}`} alt='player_2' />
+            <img src={`characters/large/${players.player_2?.image_large}`} alt='player_2' />
         </div>
     </>
   )
